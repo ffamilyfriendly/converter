24bd3606beb8e8e2c172bdf166e7330175d528e7
ffamilyfriendly <jonathan.hermin@skola.sala.se>
2021-08-25 18:10:20 +0200

    Prototype Interaction handler

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-const config = require("dotenv").config()
+const config = require("dotenv").config().parsed,
+    { Interaction, InteractionHandler, InteractionResponse } = require("./lib/interactions")
+
+const handler = new InteractionHandler(config.TOKEN,"880097206818988043")
+const command = new Interaction("MESSAGE","Auto Convert")
+
+/**
+ * 
+ * @param {InteractionResponse} res 
+ */
+command.onUsed = (res) => {
+    res.content = "works!"
+    res.respond()
+}
+handler.handle(command)
+
+// command.addToDiscord("874566459429355581")
+//     .then(res => console.log(res))
+//     .catch(err => console.error(err))
--- readme.md
+++ readme.md
@@ -1,2 +1,5 @@
 # Converter
 adds 
+
+## my instance
+https://discord.com/api/oauth2/authorize?client_id=880097206818988043&permissions=0&scope=bot%20applications.commands
